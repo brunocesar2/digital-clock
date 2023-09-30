ea98a93e8f6657ec07dcfc53b5b2e489cc52c2ea
unknown <brunocesar.tenorio1@gmail.com>
2023-09-30 11:32:56 -0300

    initial commit

---
+++ js/script.js
@@ -0,0 +1,19 @@
+const hours = document.querySelector("[data-hours]");
+const minutes = document.querySelector("[data-minutes]");
+const seconds = document.querySelector("[data-seconds]");
+if (hours && minutes && seconds) {
+  function clock() {
+    const currentDate = new Date();
+    const currentHour = currentDate.getHours();
+    const currentMinutes = currentDate.getMinutes();
+    const currentSeconds = currentDate.getSeconds();
+
+    hours.innerHTML = formatTime(currentHour);
+    minutes.innerHTML = formatTime(currentMinutes);
+    seconds.innerHTML = formatTime(currentSeconds);
+  }
+  function formatTime(time) {
+    return time < 10 ? `0${time}` : `${time}`;
+  }
+  setInterval(clock, 1000);
+}
